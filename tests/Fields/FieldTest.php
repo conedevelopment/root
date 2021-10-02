5cbd8fc0c84abe2001b5e7e1b706e4499a58f7fd
D. Nagy Gergő <hello@iamgergo.com>
2021-10-02 15:29:17 +0200

    more tests

--- tests/Fields/FieldTest.php
+++ tests/Fields/FieldTest.php
@@ -17,7 +17,7 @@ class FieldTest extends TestCase
     }
 
     /** @test */
-    public function it_gets_attributes()
+    public function a_field_gets_attributes()
     {
         $this->assertSame('name', $this->field->name);
         $this->assertSame('name', $this->field->getAttribute('name'));
@@ -26,5 +26,31 @@ class FieldTest extends TestCase
             ['label' => 'Name', 'name' => 'name', 'id' => 'name'],
             $this->field->getAttributes()
         );
+    }
+
+    /** @test */
+    public function a_field_checks_attributes()
+    {
+        $this->assertTrue(isset($this->field->name));
+        $this->assertTrue($this->field->hasAttribute('name'));
+
+        $this->assertFalse(isset($this->field->foo));
+        $this->assertFalse($this->field->hasAttribute('foo'));
+    }
+
+    /** @test */
+    public function a_field_sets_attributes()
+    {
+        $this->assertFalse($this->field->hasAttribute('min'));
+        $this->assertFalse($this->field->hasAttribute('max'));
+
+        $this->field->min = 10;
+        $this->field->setAttribute('max', 30);
+
+        $this->assertTrue($this->field->hasAttribute('min'));
+        $this->assertTrue($this->field->hasAttribute('max'));
+
+        $this->assertSame(10, $this->field->getAttribute('min'));
+        $this->assertSame(30, $this->field->getAttribute('max'));
     }
 }
