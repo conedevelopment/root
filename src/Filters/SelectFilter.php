6bc88fc59c0bab11ea3a578312465b7686145c2a
D. Nagy Gergő <hello@iamgergo.com>
2021-10-09 10:02:56 +0200

    filters

--- src/Filters/SelectFilter.php
+++ src/Filters/SelectFilter.php
@@ -2,7 +2,23 @@
 
 namespace Cone\Root\FIlters;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
 class SelectFilter extends Filter
 {
-    //
+    /**
+     * Apply the filter on the query.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $value
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function apply(Builder $query, Request $request, mixed $value): Builder
+    {
+        //
+
+        return parent::apply($query, $request, $value);
+    }
 }
