29c30b29c2f90492bb2b3c08efa26efb00595f56
D. Nagy Gergő <hello@iamgergo.com>
2021-10-11 17:34:18 +0200

    prepare middleware

---
+++ src/Http/Middleware/HandleRootRequests.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Cone\Root\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Inertia\Middleware;
+
+class HandleRootRequests extends Middleware
+{
+    /**
+     * The root template that's loaded on the first page visit.
+     *
+     * @var string
+     */
+    protected string $rootView = 'app';
+
+    /**
+     * Defines the props that are shared by default.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function share(Request $request): array
+    {
+        return array_merge(parent::share($request), [
+            'csrf_token' => static function () use ($request): string {
+                return $request->session()->token();
+            },
+        ]);
+    }
+}
