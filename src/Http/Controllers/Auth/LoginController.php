e0a5a07eae579e53ddea40a7ed736d46c83c55c8
D. Nagy Gergő <hello@iamgergo.com>
2022-01-13 12:55:22 +0100

    cleanup auth views

--- src/Http/Controllers/Auth/LoginController.php
+++ src/Http/Controllers/Auth/LoginController.php
@@ -4,218 +4,54 @@ namespace Cone\Root\Http\Controllers\Auth;
 
 use Cone\Root\Http\Controllers\Controller;
 use Cone\Root\Http\Requests\Auth\LoginRequest;
-use Illuminate\Auth\Events\Lockout;
-use Illuminate\Cache\RateLimiter;
-use Illuminate\Contracts\View\View as ViewResponse;
+use Illuminate\Contracts\View\View as ViewContract;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Redirect;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\View;
-use Illuminate\Support\Str;
-use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
     /**
-     * The maxiumum login attempts.
-     *
-     * @var int
-     */
-    public const MAX_ATTEMPTS = 5;
-
-    /**
-     * The login attempt decay minutes.
-     *
-     * @var int
-     */
-    public const DECAY_MINUTES = 1;
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('guest')->except('logout');
-    }
-
-    /**
-     * Show the application's login form.
+     * Display the login view.
      *
      * @return \Illuminate\Contracts\View\View
      */
-    public function show(): ViewResponse
+    public function create(): ViewContract
     {
         return View::make('root::auth.login');
     }
 
     /**
-     * Handle a login request to the application.
+     * Handle an incoming authentication request.
      *
-     * @param  \Cone\Root\Http\Requests\LoginRequest  $request
-     * @return \Illuminate\Http\RedirectResponse
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    public function login(LoginRequest $request): RedirectResponse
-    {
-        if ($this->hasTooManyLoginAttempts($request)) {
-            $this->fireLockoutEvent($request);
-
-            $this->sendLockoutResponse($request);
-        }
-
-        if (! $this->attemptLogin($request)) {
-            $this->incrementLoginAttempts($request);
-
-            throw ValidationException::withMessages([
-                'email' => [__('auth.failed')],
-            ]);
-        }
-
-        return $this->sendLoginResponse($request);
-    }
-
-    /**
-     * Attempt to log the user into the application.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    protected function attemptLogin(Request $request): bool
-    {
-        return Auth::guard()->attempt(
-            $request->only(['email', 'password']),
-            $request->filled('remember')
-        );
-    }
-
-    /**
-     * Send the response after the user was authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\Auth\LoginRequest  $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    protected function sendLoginResponse(Request $request): RedirectResponse
+    public function store(LoginRequest $request): RedirectResponse
     {
+        $request->authenticate();
+
         $request->session()->regenerate();
 
-        $this->clearLoginAttempts($request);
-
-        return Redirect::intended(URL::route('root.dashboard'));
+        return Redirect::intended('root.dashboard');
     }
 
     /**
-     * Log the user out of the application.
+     * Destroy an authenticated session.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function logout(Request $request): RedirectResponse
+    public function destroy(Request $request): RedirectResponse
     {
-        Auth::guard()->logout();
+        Auth::guard('web')->logout();
 
         $request->session()->invalidate();
 
         $request->session()->regenerateToken();
 
         return Redirect::route('root.login');
-    }
-
-    /**
-     * Determine if the user has too many failed login attempts.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return bool
-     */
-    protected function hasTooManyLoginAttempts(Request $request): bool
-    {
-        return $this->limiter()->tooManyAttempts(
-            $this->throttleKey($request), static::MAX_ATTEMPTS
-        );
-    }
-
-    /**
-     * Increment the login attempts for the user.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
-     */
-    protected function incrementLoginAttempts(Request $request): void
-    {
-        $this->limiter()->hit(
-            $this->throttleKey($request), static::DECAY_MINUTES * 60
-        );
-    }
-
-    /**
-     * Redirect the user after determining they are locked out.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
-    protected function sendLockoutResponse(Request $request): void
-    {
-        $seconds = $this->limiter()->availableIn(
-            $this->throttleKey($request)
-        );
-
-        throw ValidationException::withMessages([
-            'email' => [__('auth.throttle', [
-                'seconds' => $seconds,
-                'minutes' => ceil($seconds / 60),
-            ])],
-        ])->status(Response::HTTP_TOO_MANY_REQUESTS);
-    }
-
-    /**
-     * Clear the login locks for the given user credentials.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
-     */
-    protected function clearLoginAttempts(Request $request): void
-    {
-        $this->limiter()->clear($this->throttleKey($request));
-    }
-
-    /**
-     * Fire an event when a lockout occurs.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return void
-     */
-    protected function fireLockoutEvent(Request $request): void
-    {
-        Event::dispatch(new Lockout($request));
-    }
-
-    /**
-     * Get the throttle key for the given request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string
-     */
-    protected function throttleKey(Request $request): string
-    {
-        return Str::lower($request->input('email')).'|'.$request->ip();
-    }
-
-    /**
-     * Get the rate limiter instance.
-     *
-     * @return \Illuminate\Cache\RateLimiter
-     */
-    protected function limiter(): RateLimiter
-    {
-        return App::make(RateLimiter::class);
     }
 }
--- src/Http/Requests/Auth/LoginRequest.php
+++ src/Http/Requests/Auth/LoginRequest.php
@@ -2,7 +2,13 @@
 
 namespace Cone\Root\Http\Requests\Auth;
 
+use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
 
 class LoginRequest extends FormRequest
 {
@@ -27,5 +33,62 @@ class LoginRequest extends FormRequest
             'email' => ['required', 'string', 'email'],
             'password' => ['required', 'string'],
         ];
+    }
+
+    /**
+     * Attempt to authenticate the request's credentials.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function authenticate(): void
+    {
+        $this->ensureIsNotRateLimited();
+
+        if (! Auth::attempt($this->only('email', 'password'), $this->boolean('remember'))) {
+            RateLimiter::hit($this->throttleKey());
+
+            throw ValidationException::withMessages([
+                'email' => __('auth.failed'),
+            ]);
+        }
+
+        RateLimiter::clear($this->throttleKey());
+    }
+
+    /**
+     * Ensure the login request is not rate limited.
+     *
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function ensureIsNotRateLimited(): void
+    {
+        if (! RateLimiter::tooManyAttempts($this->throttleKey(), 5)) {
+            return;
+        }
+
+        Event::dispatch(new Lockout($this));
+
+        $seconds = RateLimiter::availableIn($this->throttleKey());
+
+        throw ValidationException::withMessages([
+            'email' => trans('auth.throttle', [
+                'seconds' => $seconds,
+                'minutes' => ceil($seconds / 60),
+            ]),
+        ]);
+    }
+
+    /**
+     * Get the rate limiting throttle key for the request.
+     *
+     * @return string
+     */
+    public function throttleKey(): string
+    {
+        return Str::lower($this->input('email')).'|'.$this->ip();
     }
 }
