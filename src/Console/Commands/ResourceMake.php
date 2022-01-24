028f6e9006facd12df0b0ad090b984f2e16af6ca
D. Nagy Gergő <hello@iamgergo.com>
2022-01-24 16:07:55 +0100

    cleanup

--- src/Console/Commands/ResourceMake.php
+++ src/Console/Commands/ResourceMake.php
@@ -3,7 +3,6 @@
 namespace Cone\Root\Console\Commands;
 
 use Illuminate\Console\GeneratorCommand;
-use Symfony\Component\Console\Input\InputOption;
 
 class ResourceMake extends GeneratorCommand
 {
@@ -47,46 +46,5 @@ class ResourceMake extends GeneratorCommand
     protected function getDefaultNamespace($rootNamespace): string
     {
         return $rootNamespace.'\\Root\\Resources';
-    }
-
-    /**
-     * Build the class with the given name.
-     *
-     * @param  string  $name
-     * @return string
-     */
-    protected function buildClass($name): string
-    {
-        return str_replace(
-            ['QualifiedDummyModelClass', 'DummyModelClass'],
-            [$this->qualifyModel($this->getModelClass()), $this->getModelClass()],
-            parent::buildClass($name)
-        );
-    }
-
-    /**
-     * Get the model class.
-     *
-     * @return string
-     */
-    protected function getModelClass(): string
-    {
-        if (! $model = $this->option('model')) {
-            $model = str_replace('Resource', '', class_basename(static::class));
-        }
-
-        return $model;
-    }
-
-    /**
-     * Get the console command options.
-     *
-     * @return array
-     */
-    protected function getOptions(): array
-    {
-        return [
-            ['model', null, InputOption::VALUE_OPTIONAL, 'The model class'],
-        ];
     }
 }
