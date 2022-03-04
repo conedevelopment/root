f13047b6af54a4e85332fe0a4a8f58d52a715c57
D. Nagy Gergő <hello@iamgergo.com>
2022-03-04 17:35:13 +0100

    fix

--- src/RootServiceProvider.php
+++ src/RootServiceProvider.php
@@ -169,7 +169,8 @@ class RootServiceProvider extends ServiceProvider
     {
         $this->app['router']->macro('prependGroupStackPrefix', function (string $prefix): Router {
             if (Root::getPath() === '') {
-                $attributes = $this->mergeWithLastGroup(['prefix' => $prefix], true);
+                $attributes = $this->mergeWithLastGroup(['prefix' => $prefix], false);
+                dd($attributes);
             } else {
                 $attributes = [
                     'prefix' => Str::replaceFirst(
