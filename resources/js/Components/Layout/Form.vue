e3fb4bd76fb6c7c24faa1c365f649a28dd01bd77
Adam Laki <info@adamlaki.com>
2022-03-13 09:29:19 +0100

    Remove form layout two column grid variant

--- resources/js/Components/Layout/Form.vue
+++ resources/js/Components/Layout/Form.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="app-card card card--body">
-        <div class="card__header">
-            <h2 class="card__title">General</h2>
-        </div>
-        <div class="card__inner">
-            <slot></slot>
+    <div class="form-layout">
+        <div class="app-card card">
+            <div class="card__header">
+                <h2 class="card__title">General</h2>
+            </div>
+            <div class="card__inner">
+                <slot></slot>
+            </div>
         </div>
     </div>
 </template>
