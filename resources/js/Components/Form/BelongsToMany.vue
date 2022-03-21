854216f88f1be5d4ecdd656fd07a3c62afe7537d
Adam Laki <info@adamlaki.com>
2022-03-21 17:58:39 +0100

    Style accordion

--- resources/js/Components/Form/BelongsToMany.vue
+++ resources/js/Components/Form/BelongsToMany.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="form-group" :class="class" :style="style">
-        <div class="form-row--mixed">
+        <div class="form-row--mixed form-row--accordion">
             <FormHandler
                 v-bind="$attrs"
                 v-model="value"
@@ -9,20 +9,31 @@
                 :name="name"
                 :select-resolver="selectResolver"
             ></FormHandler>
-            <div>
+            <div class="accordion-wrapper">
                 <div v-for="(pivot, key) in modelValue" :key="key">
-                    <fieldset v-if="pivotFields?.[key] && pivotFields[key].length > 0">
-                        <legend>{{ formattedValue[key] }}</legend>
-                        <FormHandler
-                            v-for="field in pivotFields[key]"
-                            v-bind="field"
-                            v-model="modelValue[key][field.name]"
-                            :form="$parent.form"
-                            :key="`${key}-${field.name}`"
-                            :id="`${name}.${key}.${field.name}`"
-                            :name="`${name}.${key}.${field.name}`"
-                        ></FormHandler>
-                    </fieldset>
+                    <div class="accordion" v-if="pivotFields?.[key] && pivotFields[key].length > 0">
+                        <h2 class="accordion__title">
+                            <button type="button" aria-expanded="true">
+                                {{ formattedValue[key] }}
+                                <svg aria-hidden="true" focusable="false" height="24px" viewBox="0 0 24 24" width="24px">
+                                    <path d="M12,2c-5.52,0 -10,4.48 -10,10c0,5.52 4.48,10 10,10c5.52,0 10,-4.48 10,-10c0,-5.52 -4.48,-10 -10,-10Zm0,18c-4.41,0 -8,-3.59 -8,-8c0,-4.41 3.59,-8 8,-8c4.41,0 8,3.59 8,8c0,4.41 -3.59,8 -8,8Z" fill="currentColor"></path>
+                                    <path d="M7,12c0,0.55 0.45,1 1,1l8,0c0.55,0 1,-0.45 1,-1c0,-0.55 -0.45,-1 -1,-1l-8,0c-0.55,0 -1,0.45 -1,1Z" fill="currentColor"></path>
+                                    <path class="vert" d="M12,7c-0.55,0 -1,0.45 -1,1l0,8c0,0.55 0.45,1 1,1c0.55,0 1,-0.45 1,-1l0,-8c0,-0.55 -0.45,-1 -1,-1Z" fill="currentColor"></path>
+                                </svg>
+                            </button>
+                        </h2>
+                        <div class="accordion__content form-group-stack">
+                            <FormHandler
+                                v-for="field in pivotFields[key]"
+                                v-bind="field"
+                                v-model="modelValue[key][field.name]"
+                                :form="$parent.form"
+                                :key="`${key}-${field.name}`"
+                                :id="`${name}.${key}.${field.name}`"
+                                :name="`${name}.${key}.${field.name}`"
+                            ></FormHandler>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
