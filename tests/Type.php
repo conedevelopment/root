e62bb6c2011d3459c82d86b04025a977b3d73e4d
D. Nagy Gergő <hello@iamgergo.com>
2022-04-07 13:55:20 +0200

    fix tests

--- tests/Type.php
+++ tests/Type.php
@@ -3,10 +3,16 @@
 namespace Cone\Root\Tests;
 
 use Cone\Root\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 class Type extends SelectFilter
 {
+    public function apply(Request $request, Builder $query, mixed $value): Builder
+    {
+        return $query;
+    }
+
     public function options(Request $request): array
     {
         return [];
