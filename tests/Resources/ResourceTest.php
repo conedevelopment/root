28fb5ed54419bf8e67bc21061642359650ec7097
D. Nagy Gergő <hello@iamgergo.com>
2022-05-31 19:44:19 +0200

    wip

--- tests/Resources/ResourceTest.php
+++ tests/Resources/ResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace Cone\Root\Tests\Extracts;
 
+use Cone\Root\Http\Requests\CreateRequest;
 use Cone\Root\Http\Requests\IndexRequest;
 use Cone\Root\Resources\Resource;
 use Cone\Root\Tests\Post;
@@ -16,6 +17,16 @@ class ResourceTest extends TestCase
         parent::setUp();
 
         $this->resource = new Resource(Post::class);
+    }
+
+    /** @test */
+    public function a_resource_has_icon()
+    {
+        $this->assertSame('inventory-2', $this->resource->getIcon());
+
+        $this->resource->icon('fake-icon');
+
+        $this->assertSame('fake-icon', $this->resource->getIcon());
     }
 
     /** @test */
@@ -35,6 +46,27 @@ class ResourceTest extends TestCase
                 'page.component' => 'Resources/Index',
                 'page.props' => function ($props) use ($request) {
                     return empty(array_diff_key($this->resource->toIndex($request), $props));
+                },
+            ]);
+    }
+
+    /** @test */
+    public function a_resource_has_create_representation()
+    {
+        $request = CreateRequest::createFrom($this->request);
+
+        $request->setRouteResolver(function () {
+            return $this->app['router']->getRoutes()->getByName('root.posts.create');
+        });
+
+        $this->actingAs($this->admin)
+            ->get('/root/posts/create')
+            ->assertOk()
+            ->assertViewIs('root::app')
+            ->assertViewHas([
+                'page.component' => 'Resources/Form',
+                'page.props' => function ($props) use ($request) {
+                    return empty(array_diff_key($this->resource->toCreate($request), $props));
                 },
             ]);
     }
