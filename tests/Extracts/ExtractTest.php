6278efd79c6dfa6170e70f8c8256b13f7dfae05e
D. Nagy Gergő <hello@iamgergo.com>
2022-06-24 23:03:04 +0200

    tests

--- tests/Extracts/ExtractTest.php
+++ tests/Extracts/ExtractTest.php
@@ -3,7 +3,16 @@
 namespace Cone\Root\Tests\Extracts;
 
 use Cone\Root\Exceptions\QueryResolutionException;
+use Cone\Root\Fields\Text;
+use Cone\Root\Support\Collections\Actions;
+use Cone\Root\Support\Collections\Fields;
+use Cone\Root\Support\Collections\Filters;
+use Cone\Root\Support\Collections\Widgets;
+use Cone\Root\Tests\Actions\PublishPosts;
+use Cone\Root\Tests\Filters\Published;
+use Cone\Root\Tests\Post;
 use Cone\Root\Tests\TestCase;
+use Cone\Root\Tests\Widgets\PostsCount;
 
 class ExtractTest extends TestCase
 {
@@ -17,10 +26,102 @@ class ExtractTest extends TestCase
     }
 
     /** @test */
-    public function an_action_can_throw_query_resolution_exception()
+    public function an_extract_has_key()
+    {
+        $this->assertSame('long-posts', $this->extract->getKey());
+    }
+
+    /** @test */
+    public function an_extract_has_name()
+    {
+        $this->assertSame('Long Posts', $this->extract->getName());
+    }
+
+    /** @test */
+    public function an_extract_registers_routes()
+    {
+        $this->app['router']->prefix('posts/extracts')->group(function ($router) {
+            $this->extract->registerRoutes($this->request, $router);
+        });
+
+        $this->assertSame('posts/extracts/long-posts', $this->extract->getUri());
+
+        $this->assertArrayHasKey(
+            $this->extract->getUri(),
+            $this->app['router']->getRoutes()->get('GET')
+        );
+    }
+
+    /** @test */
+    public function an_extract_resolves_actions()
+    {
+        $this->extract->withActions([
+            PublishPosts::make(),
+        ]);
+
+        $this->assertSame(
+            Actions::make(array_merge($this->extract->actions($this->request), [PublishPosts::make()]))->toArray(),
+            $this->extract->resolveActions($this->request)->toArray()
+        );
+    }
+
+    /** @test */
+    public function an_extract_resolves_filters()
+    {
+        $this->extract->withFilters([
+            Published::make(),
+        ]);
+
+        $this->assertSame(
+            Filters::make(array_merge($this->extract->filters($this->request), [Published::make()]))->toArray(),
+            $this->extract->resolveFilters($this->request)->toArray()
+        );
+    }
+
+    /** @test */
+    public function an_extract_resolves_fields()
+    {
+        $this->extract->withFields([
+            Text::make(__('Name')),
+        ]);
+
+        $this->assertSame(
+            Fields::make(array_merge($this->extract->fields($this->request), [Text::make(__('Name'))]))->toArray(),
+            $this->extract->resolveFields($this->request)->toArray()
+        );
+    }
+
+    /** @test */
+    public function an_extract_resolves_widgets()
+    {
+        $this->extract->withWidgets([
+            PostsCount::make(),
+        ]);
+
+        $this->assertSame(
+            Widgets::make(array_merge($this->extract->widgets($this->request), [PostsCount::make()]))->toArray(),
+            $this->extract->resolveWidgets($this->request)->toArray()
+        );
+    }
+
+    /** @test */
+    public function an_extract_resolvers_query()
     {
         $this->expectException(QueryResolutionException::class);
 
-        $this->extract->resolveQuery($this->request);
+        try {
+            $this->extract->resolveQuery($this->request);
+        } finally {
+            //
+        }
+
+        $this->extract->withQuery(function () {
+            return Post::query();
+        });
+
+        $this->assertSame(
+            Post::query()->toSql(),
+            $this->extract->resolveQuery($this->request)->toSql()
+        );
     }
 }
--- tests/Extracts/LongPosts.php
+++ tests/Extracts/LongPosts.php
@@ -3,39 +3,8 @@
 namespace Cone\Root\Tests\Extracts;
 
 use Cone\Root\Extracts\Extract;
-use Cone\Root\Fields\Text;
-use Cone\Root\Http\Requests\RootRequest;
-use Cone\Root\Tests\Actions\PublishPosts;
-use Cone\Root\Tests\Filters\Published;
-use Cone\Root\Tests\Widgets\PostsCount;
 
 class LongPosts extends Extract
 {
-    public function filters(RootRequest $request): array
-    {
-        return array_merge(parent::filters($request), [
-            Published::make()->multiple(),
-        ]);
-    }
-
-    public function actions(RootRequest $request): array
-    {
-        return [
-            PublishPosts::make(),
-        ];
-    }
-
-    public function fields(RootRequest $request): array
-    {
-        return [
-            Text::make('Title')->sortable()->searchable(),
-        ];
-    }
-
-    public function widgets(RootRequest $request): array
-    {
-        return [
-            PostsCount::make(),
-        ];
-    }
+    //
 }
