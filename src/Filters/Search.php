cd1f76df466557f6c46909bf0bc05f9f51c8bc6a
Viktor Szépe <viktor@szepe.net>
2022-07-05 20:27:39 +0000

    Treat attributes only as arrays

--- src/Filters/Search.php
+++ src/Filters/Search.php
@@ -53,21 +53,19 @@ class Search extends Filter
 
         return $query->where(static function (Builder $query) use ($attributes, $value): void {
             foreach ($attributes as $attribute => $columns) {
-                $boolean = array_key_first($attributes) === $attribute ? 'and' : 'or';
-
-                if (is_array($columns)) {
-                    $query->has($attribute, '>=', 1, $boolean, static function (Builder $query) use ($columns, $value): Builder {
-                        foreach ($columns as $column) {
-                            $boolean = $columns[0] === $column ? 'and' : 'or';
-
-                            $query->where($query->qualifyColumn($column), 'like', "%{$value}%", $boolean);
-                        }
-
-                        return $query;
-                    });
-                } else {
-                    $query->where($query->qualifyColumn($attribute), 'like', "%{$value}%", $boolean);
+                if (! is_array($columns)) {
+                    $columns = [$columns];
                 }
+
+                $query->has($attribute, '>=', 1, $boolean, static function (Builder $query) use ($columns, $value): Builder {
+                    foreach ($columns as $column) {
+                        $boolean = $columns[0] === $column ? 'and' : 'or';
+
+                        $query->where($query->qualifyColumn($column), 'like', "%{$value}%", $boolean);
+                    }
+
+                    return $query;
+                });
             }
         });
     }
