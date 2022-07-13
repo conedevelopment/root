9b61e111ae721d0120077b406886529be1e29351
D. Nagy Gergő <hello@iamgergo.com>
2022-07-13 11:32:11 +0200

    Meta tests

--- tests/Models/MetaTest.php
+++ tests/Models/MetaTest.php
@@ -5,21 +5,61 @@ namespace Cone\Root\Tests\Models;
 use Cone\Root\Models\Medium;
 use Cone\Root\Models\Meta;
 use Cone\Root\Tests\TestCase;
+use Cone\Root\Traits\HasMeta;
 
 class MetaTest extends TestCase
 {
+    protected $model;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->model = new class(Medium::factory()->make()->toArray()) extends Medium {
+            use HasMeta;
+
+            protected $casts = [
+                'metas.price' => 'float',
+                'properties' => 'json',
+            ];
+        };
+
+        $this->model->save();
+    }
+
     /** @test */
     public function a_meta_belongs_to_a_metable()
     {
         $meta = Meta::factory()->make();
 
-        $medium = Medium::factory()->create();
+        $meta->metable()->associate($this->model)->save();
 
-        $meta->metable()->associate($medium)->save();
+        $this->assertTrue($meta->metable->is($this->model));
+    }
 
-        $this->assertSame(
-            [Medium::class, $medium->id],
-            [$meta->metable_type, $meta->metable_id]
+    /** @test */
+    public function a_metable_model_has_metas()
+    {
+        $this->model->metas()->save(
+            $meta = Meta::factory()->make()
+        );
+
+        $this->assertTrue($this->model->metas->first()->is($meta));
+    }
+
+    /** @test */
+    public function a_metable_model_proxies_meta_attributes()
+    {
+        $this->assertNull($this->model->getAttribute('metas.price'));
+
+        $this->model->setAttribute('metas.price', 10.3);
+
+        $this->assertSame(10.3, $this->model->getAttribute('metas.price'));
+
+        $this->model->save();
+
+        $this->assertTrue(
+            $this->model->metas->contains(fn ($meta) => $meta->key === 'price')
         );
     }
 }
