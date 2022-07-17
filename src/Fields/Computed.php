7a09ed6ed6534828cdea1fb7cf4325fcf29398ac
D. Nagy Gergő <hello@iamgergo.com>
2022-07-17 22:53:54 +0200

    Computed field

---
+++ src/Fields/Computed.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Cone\Root\Fields;
+
+use Closure;
+use Cone\Root\Http\Requests\RootRequest;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Computed extends Field
+{
+    /**
+     * Create a new field instance.
+     *
+     * @param  string  $label
+     * @param  \Closure  $callback
+     * @return void
+     */
+    public function __construct(string $label, Closure $callback)
+    {
+        parent::__construct($label, Str::random(6));
+
+        $this->value($callback);
+        $this->visibleOnDisplay();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function sortable(bool|Closure $value = true): static
+    {
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function searchable(bool|Closure $value = true): static
+    {
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function persist(RootRequest $request, Model $model): void
+    {
+        //
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function resolveHydrate(RootRequest $request, Model $model, mixed $value): void
+    {
+        //
+    }
+}
