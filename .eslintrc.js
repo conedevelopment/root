c1f07b0711938ff738ffa1343ddaf34b5dd7ae26
Adam Laki <info@adamlaki.com>
2022-08-11 14:52:22 +0200

    Modify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,30 @@ module.exports = {
         "plugin:vue/strongly-recommended",
         "plugin:vue/recommended"
     ],
-    rules: {}
+    "rules": {
+        "vue/html-indent": ["error", 4, {
+            "attribute": 1,
+            "baseIndent": 1,
+            "closeBracket": 0,
+            "alignAttributesVertically": true,
+            "ignores": []
+        }],
+        "vue/html-self-closing": ["error", {
+            "html": {
+                "void": "any",
+                "normal": "any",
+                "component": "any"
+            },
+            "svg": "any",
+            "math": "any"
+        }],
+        "vue/max-attributes-per-line": ["error", {
+            "singleline": {
+                "max": 5
+            },
+            "multiline": {
+                "max": 5
+            }
+        }]
+    }
 }
