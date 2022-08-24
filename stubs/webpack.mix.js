37bc12d494e4fb24d7aac2cdbad609ca5be7461f
D. Nagy Gergő <hello@iamgergo.com>
2022-08-24 08:08:37 +0200

    cleanup

--- stubs/webpack.mix.js
+++
@@ -1,6 +0,0 @@
-mix.js('resources/js/vendor/root/app.js', 'public/vendor/root')
-    .vue({ runtimeOnly: true })
-    .sass('resources/sass/vendor/root/app.scss', 'public/vendor/root')
-    .options({ processCssUrls: false })
-    .sourceMaps()
-    .version();
