d8ea4e90c292ffafb99c83ca94da3dc6ba19197d
D. Nagy Gergő <hello@iamgergo.com>
2022-09-01 17:16:03 +0200

    wip

--- src/Traits/ResolvesBreadcrumbs.php
+++ src/Traits/ResolvesBreadcrumbs.php
@@ -3,21 +3,9 @@
 namespace Cone\Root\Traits;
 
 use Cone\Root\Http\Requests\RootRequest;
-use Illuminate\Support\Str;
 
 trait ResolvesBreadcrumbs
 {
-    /**
-     * Resolve the title for the given request.
-     *
-     * @param  \Cone\Root\Http\Requests\RootRequest  $request
-     * @return string
-     */
-    public function resolveTitle(RootRequest $request): string
-    {
-        return Str::headline(class_basename(static::class));
-    }
-
     /**
      * Resolve the breadcrumbs for the given request.
      *
