88d72a45a6b8fcfb6d6376802002d84a721ce375
D. Nagy Gergő <hello@iamgergo.com>
2023-02-01 09:30:58 +0100

    wip

--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
+            refresh: true,
         }),
         vue({
             template: {
