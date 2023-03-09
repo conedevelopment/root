676a9931a25d27da71889a45c10cde7609c52559
D. Nagy Gergő <hello@iamgergo.com>
2023-03-09 10:56:25 +0100

    wip

--- src/Application.php
+++ src/Application.php
@@ -2,14 +2,22 @@
 
 namespace Cone\Root;
 
+use Cone\Root\Support\Collections\Resources;
 use Illuminate\Contracts\Foundation\Application as Container;
 
 class Application
 {
+    public const VERSION = '1.2.0';
+
     /**
      * The Laravel application instance.
      */
     protected Container $app;
+
+    /**
+     * The resources collection.
+     */
+    public readonly Resources $resources;
 
     /**
      * Create a new Root application instance.
@@ -17,5 +25,14 @@ class Application
     public function __construct(Container $app)
     {
         $this->app = $app;
+        $this->resources = new Resources();
+    }
+
+    /**
+     * Boot the application.
+     */
+    public function boot(): void
+    {
+        //
     }
 }
