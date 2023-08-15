9a7b336541a37ed57d05e57ed6a6c5a22162edd1
D. Nagy Gergő <hello@iamgergo.com>
2023-08-15 14:11:28 +0200

    fix class name

--- src/View/Components/Notifications.php
+++ src/View/Components/Notifications.php
@@ -5,7 +5,7 @@ namespace Cone\Root\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
-class Notirications extends Component
+class Notifications extends Component
 {
     /**
      * Get the view / view contents that represent the component.
