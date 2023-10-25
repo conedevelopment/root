aacb3d0f27d819108c035ba0d95a56b15cc7dfcf
D. Nagy Gergő <hello@iamgergo.com>
2023-10-25 13:53:21 +0200

    fix response

--- src/Http/Controllers/ActionController.php
+++ src/Http/Controllers/ActionController.php
@@ -2,16 +2,16 @@
 
 namespace Cone\Root\Http\Controllers;
 
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Symfony\Component\HttpFoundation\Response;
 
 class ActionController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request): RedirectResponse
+    public function __invoke(Request $request): Response
     {
         $action = $request->route('action');
 
