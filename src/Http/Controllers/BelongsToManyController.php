eb37d138e9e1f08d181cee1b420f88617403cade
D. Nagy Gergő <hello@iamgergo.com>
2023-11-20 12:36:03 +0100

    wip

--- src/Http/Controllers/BelongsToManyController.php
+++ src/Http/Controllers/BelongsToManyController.php
@@ -11,22 +11,20 @@ use Illuminate\Support\Facades\Redirect;
 class BelongsToManyController extends RelationController
 {
     /**
-     * Store a newly created resource in storage.
+     * {@inheritdoc}
      */
     public function store(Request $request, Model $model): RedirectResponse
     {
         $field = $request->route('field');
 
-        dd($request->all());
-
         $field->handleFormRequest($request, $model);
 
         return Redirect::to('')
-                    ->with('alerts.relation-created', Alert::success(__('The relation has been created!')));
+            ->with('alerts.relation-created', Alert::success(__('The relation has been created!')));
     }
 
     /**
-     * Update the specified resource in storage.
+     * {@inheritdoc}
      */
     public function update(Request $request, Model $model, Model $related): RedirectResponse
     {
@@ -35,17 +33,17 @@ class BelongsToManyController extends RelationController
         $field->handleFormRequest($request, $model);
 
         return Redirect::back()
-                    ->with('alerts.relation-updated', Alert::success(__('The relation has been updated!')));
+            ->with('alerts.relation-updated', Alert::success(__('The relation has been updated!')));
     }
 
     /**
-     * Remove the specified resource from storage.
+     * {@inheritdoc}
      */
     public function destroy(Request $request, Model $model, Model $related): RedirectResponse
     {
         //
 
         return Redirect::to('')
-                    ->with('alerts.relation-deleted', Alert::success(__('The relation has been deleted!')));
+            ->with('alerts.relation-deleted', Alert::success(__('The relation has been deleted!')));
     }
 }
