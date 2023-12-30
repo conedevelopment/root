a63950644d0c7aff1af9e5e2f4ba5afc2a863c08
D. Nagy Gergő <hello@iamgergo.com>
2023-12-30 11:31:15 +0100

    tests

--- tests/Models/MediumTest.php
+++ tests/Models/MediumTest.php
@@ -2,17 +2,88 @@
 
 namespace Cone\Root\Tests\Models;
 
+use Cone\Root\Models\Medium;
 use Cone\Root\Tests\TestCase;
+use Cone\Root\Tests\User;
 
 class MediumTest extends TestCase
 {
+    protected User $user;
+
+    protected Medium $medium;
+
     public function setUp(): void
     {
         parent::setUp();
+
+        $this->user = User::factory()->create();
+
+        $this->medium = Medium::factory()->create();
     }
 
-    public function test_a_medium_belongs_to_user()
+    public function test_a_medium_belongs_to_user(): void
     {
-        $this->assertTrue(true);
+        $this->medium->user()->associate($this->user)->save();
+
+        $this->assertTrue($this->medium->user->is($this->user));
+    }
+
+    public function test_a_medium_can_determine_if_image(): void
+    {
+        $this->medium->setAttribute('mime_type', 'image/jpg');
+        $this->assertTrue($this->medium->isImage);
+
+        $this->medium->setAttribute('mime_type', 'application/pdf');
+        $this->assertFalse($this->medium->isImage);
+    }
+
+    public function test_a_medium_has_urls(): void
+    {
+        $this->assertEquals(['original'], array_keys($this->medium->urls));
+
+        $this->assertStringContainsString('-thumb', $this->medium->getUrl('thumb'));
+    }
+
+    public function test_a_medium_has_path(): void
+    {
+        $this->assertStringContainsString("{$this->medium->uuid}/{$this->medium->name}", $this->medium->getPath());
+        $this->assertStringContainsString("{$this->medium->uuid}/{$this->medium->name}", $this->medium->getAbsolutePath());
+    }
+
+    public function test_a_medium_has_search_query_scope(): void
+    {
+        $query = Medium::query()->search('test');
+        $this->assertSame(
+            'select * from "root_media" where "root_media"."name" like ?',
+            $query->toSql()
+        );
+        $this->assertSame(['%test%'], $query->getBindings());
+
+        $this->assertSame(
+            'select * from "root_media"',
+            Medium::query()->search()->toSql()
+        );
+    }
+
+    public function test_a_medium_has_type_query_scope(): void
+    {
+        $query = Medium::query()->type('file');
+        $this->assertSame(
+            'select * from "root_media" where "root_media"."mime_type" not like ?',
+            $query->toSql()
+        );
+        $this->assertSame(['image%'], $query->getBindings());
+
+        $query = Medium::query()->type('image');
+        $this->assertSame(
+            'select * from "root_media" where "root_media"."mime_type" like ?',
+            $query->toSql()
+        );
+        $this->assertSame(['image%'], $query->getBindings());
+
+        $this->assertSame(
+            'select * from "root_media"',
+            Medium::query()->type('test')->toSql()
+        );
     }
 }
---
+++ tests/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Cone\Root\Tests;
+
+use App\Models\User as Model;
+use Cone\Root\Interfaces\Models\User as RootUser;
+use Cone\Root\Traits\AsRootUser;
+use Cone\Root\Traits\HasMedia;
+use Cone\Root\Traits\HasMetaData;
+use Database\Factories\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+class User extends Model implements MustVerifyEmail, RootUser
+{
+    use AsRootUser;
+    use HasMedia;
+    use HasMetaData;
+
+    protected static function newFactory(): UserFactory
+    {
+        return new class () extends UserFactory
+        {
+            protected $model = User::class;
+        };
+    }
+}
