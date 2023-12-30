ab0793d2451201cd14900613f714eee1bd1a77c0
D. Nagy Gergő <hello@iamgergo.com>
2023-12-30 21:37:40 +0100

    wip

---
+++ tests/Fields/HiddenTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Cone\Root\Tests\Fields;
+
+use Cone\Root\Fields\Hidden;
+use Cone\Root\Tests\TestCase;
+
+class HiddenTest extends TestCase
+{
+    protected Hidden $field;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->field = new Hidden('Token');
+    }
+
+    public function test_a_hidden_field_has_hidden_type(): void
+    {
+        $this->assertSame('hidden', $this->field->getAttribute('type'));
+    }
+}
