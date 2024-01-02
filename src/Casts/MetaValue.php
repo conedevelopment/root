9202366989bafcf289615be872d409bde9c8f5aa
D. Nagy Gergő <hello@iamgergo.com>
2024-01-02 21:43:28 +0100

    fix meta value cast

--- src/Casts/MetaValue.php
+++ src/Casts/MetaValue.php
@@ -15,7 +15,7 @@ class MetaValue implements CastsAttributes
      */
     public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        return json_decode($value, true) ?? $value;
+        return is_null($value) ? $value : (json_decode($value, true) ?? $value);
     }
 
     /**
