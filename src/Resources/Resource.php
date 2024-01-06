548fbd99a90fcd6d2533ed306e5fd77f1e6af234
D. Nagy Gergő <hello@iamgergo.com>
2024-01-06 11:21:44 +0100

    wip

--- src/Resources/Resource.php
+++ src/Resources/Resource.php
@@ -172,6 +172,36 @@ abstract class Resource implements Arrayable, Form
     }
 
     /**
+     * Map the resource abilities.
+     */
+    public function mapAbilities(): array
+    {
+        return [
+            'viewAny' => function (Request $request): bool {
+                return is_null($this->getPolicy()) || Gate::allows('viewAny', $this->getModel());
+            },
+            'create' => function (Request $request): bool {
+                return is_null($this->getPolicy()) || Gate::allows('create', $this->getModel());
+            },
+            'view' => function (Request $request, Model $model): bool {
+                return is_null($this->getPolicy()) || Gate::allows('view', $model);
+            },
+            'update' => function (Request $request, Model $model): bool {
+                return is_null($this->getPolicy()) || Gate::allows('update', $model);
+            },
+            'delete' => function (Request $request, Model $model): bool {
+                return is_null($this->getPolicy()) || Gate::allows('delete', $model);
+            },
+            'forceDelete' => function (Request $request, Model $model): bool {
+                return is_null($this->getPolicy()) || Gate::allows('delete', $model);
+            },
+            'restore' => function (Request $request, Model $model): bool {
+                return is_null($this->getPolicy()) || Gate::allows('delete', $model);
+            },
+        ];
+    }
+
+    /**
      * Set the relations to eagerload.
      */
     public function with(array $relations): static
