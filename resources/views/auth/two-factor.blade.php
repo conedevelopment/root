b2fbc21516d7e71ef9dfb55fa7c568fc822fa499
D. Nagy Gergő <hello@iamgergo.com>
2024-04-05 14:12:03 +0200

    wip

--- resources/views/auth/two-factor.blade.php
+++ resources/views/auth/two-factor.blade.php
@@ -3,12 +3,6 @@
 {{-- Title --}}
 @section('title', __('Two Factor Authentication'))
 
-{{-- Links --}}
-@section('links')
-    <a href="{{ URL::route('password.request') }}">{{ __('Reset password') }}</a>
-    <a href="{{ URL::route('login') }}">{{ __('Login') }}</a>
-@endsection
-
 {{-- Content --}}
 @section('content')
 <p>{{ __('To finish the two factor authentication, please use the link we sent, or request a new one!') }}</p>
@@ -16,10 +10,19 @@
     @csrf
     <div class="form-group-stack">
         <div class="form-group">
-            <button class="btn btn--primary btn--lg btn--block btn--primary-shadow">
+            <button type="submit" class="btn btn--primary btn--lg btn--block btn--primary-shadow">
                 {{ __('Resend Two Factor Authentication Link') }}
             </button>
         </div>
+    </div>
+</form>
+<span class="or-separator" aria-hidden="true">{{ __('or') }}</span>
+<form method="POST" action="{{ URL::route('root.auth.logout') }}">
+    @csrf
+    <div class="form-group-stack">
+        <button type="submit" class="btn btn--light btn--sm">
+            {{ __('Logout') }}
+        </button>
     </div>
 </form>
 @endsection
