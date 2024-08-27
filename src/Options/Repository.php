b7b0590691c7a78abe87cb4cfb9df07ad37e9ee8
D. Nagy Gergő <hello@iamgergo.com>
2024-08-27 09:02:41 +0200

    wip

--- src/Options/Repository.php
+++ src/Options/Repository.php
@@ -29,4 +29,82 @@ class Repository implements Contract
     {
         return Option::proxy()->newQuery();
     }
+
+    /**
+     * Get the option.
+     */
+    public function get(string $key, mixed $default = null, bool $refresh = false): mixed
+    {
+        if (! $refresh && $this->cache->has($key)) {
+            return $this->cache->get($key);
+        }
+
+        $option = $this->query()->where('key', $key)->first();
+
+        $value = match (true) {
+            ! is_null($option) => $option->value,
+            default => $default,
+        };
+
+        $this->cache->put($key, $value);
+
+        return $value;
+    }
+
+    /**
+     * Get the options.
+     */
+    public function getMany(array $keys, array $defaults = [], bool $refresh = false): Collection
+    {
+        $cache = $this->cache->whereIn('key', $refresh ? $keys : []);
+
+        $options = $this->query()->whereIn('key',  $cache->diffKeys($keys))->get();
+
+        return Collection::make($defaults)
+            ->merge($cache)
+            ->merge($options->pluck('value', 'key')->all());
+    }
+
+    /**
+     * Set the option.
+     */
+    public function set(string $key, mixed $value): void
+    {
+        $this->query()->updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
+
+        $this->cache->put($key, $value);
+    }
+
+    /**
+     * Set the options.
+     */
+    public function setMany(array $values): void
+    {
+        foreach ($values as $key => $value) {
+            $this->set($key, $value);
+        }
+    }
+
+    /**
+     * Delete the option.
+     */
+    public function delete(string $key): void
+    {
+        $this->query()->where('key', $key)->delete();
+
+        $this->cache->forget($key);
+    }
+
+    /**
+     * Delete the options.
+     */
+    public function deleteMany(array $keys): void
+    {
+        $this->query()->whereIn('key', $keys)->delete();
+
+        $this->cache->forget($keys);
+    }
 }
