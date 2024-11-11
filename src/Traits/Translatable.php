f0684c77fd1a65d246db4846570b676220e42bb3
D. Nagy Gergő <hello@iamgergo.com>
2024-11-11 13:44:00 +0100

    wip

--- src/Traits/Translatable.php
+++ src/Traits/Translatable.php
@@ -3,6 +3,8 @@
 namespace Cone\Root\Traits;
 
 use Cone\Root\Models\Translation;
+use Cone\Root\Models\TranslationValue;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 trait Translatable
@@ -13,5 +15,31 @@ trait Translatable
     public function translations(): MorphMany
     {
         return $this->morphMany(Translation::getProxiedClass(), 'translatable');
+    }
+
+    /**
+     * Get the translation values for the model.
+     */
+    public function translationValues(): HasManyThrough
+    {
+        return $this->hasManyThrough(TranslationValue::getProxiedClass(), Translation::getProxiedClass(), 'translatable_id')
+            ->where('root_translations.translatable_type', static::class)
+            ->select(['*', 'root_translations.language as language']);
+    }
+
+    /**
+     * Translate the value of the given key.
+     */
+    public function translate(string $key, string $language): mixed
+    {
+        $value = $this->translationValues->first(function (TranslationValue $value) use ($key, $language): bool {
+            return $value->key === $key && $value->language === $language;
+        });
+
+        if (is_null($value)) {
+            return null;
+        }
+
+        return $value->mergeCasts(['key' => $this->getCasts()[$key] ?? 'string'])->value;
     }
 }
