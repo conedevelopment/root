ba31ab5e22f93cfa9c950ee5e456bdbaf62ec0fc
D. Nagy Gergő <hello@iamgergo.com>
2024-12-18 22:56:09 +0100

    fix

--- src/Http/Controllers/DashboardController.php
+++ src/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace Cone\Root\Http\Controllers;
 
 use Cone\Root\Root;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Response as ResponseFactory;
 
@@ -11,10 +12,10 @@ class DashboardController extends Controller
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Root $root): Response
+    public function __invoke(Request $request, Root $root): Response
     {
         return ResponseFactory::view('root::dashboard', [
-            'widgets' => $root->widgets->toArray(),
+            'widgets' => $root->widgets->map->data($request)->all(),
         ]);
     }
 }
