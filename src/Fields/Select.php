2cdff2e3436e35c69bc140eea139c6c57702407f
Viktor Szépe <viktor@szepe.net>
2025-10-14 12:44:11 +0200

    fix CS

--- src/Fields/Select.php
+++ src/Fields/Select.php
@@ -7,12 +7,13 @@ namespace Cone\Root\Fields;
 use Closure;
 use Cone\Root\Filters\Filter;
 use Cone\Root\Filters\RenderableFilter;
-use function Illuminate\Support\enum_value;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
+
+use function Illuminate\Support\enum_value;
 
 class Select extends Field
 {
