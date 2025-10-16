8caa6e53d7a81559490836a9a5fbde6dc83245d5
D. Nagy Gergő <hello@iamgergo.com>
2025-10-16 09:22:51 +0200

    version

--- src/Root.php
+++ src/Root.php
@@ -27,7 +27,7 @@ class Root
      *
      * @var string
      */
-    public const string VERSION = '2.7.14';
+    public const string VERSION = '2.7.15';
 
     /**
      * The registered booting callbacks.
