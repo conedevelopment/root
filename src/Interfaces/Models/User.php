8aebc300344586bcd037b0f2f9825cc45edbc664
D. Nagy Gergő <hello@iamgergo.com>
2025-10-17 08:36:13 +0200

    cleanup

--- src/Interfaces/Models/User.php
+++ src/Interfaces/Models/User.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Cone\Root\Interfaces\Models;
 
-use Cone\Root\Models\AuthCode;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Relations\MorphMany;
@@ -35,9 +34,4 @@ interface User
      * Determine whether the object requires two factor authentication.
      */
     public function requiresTwoFactorAuthentication(): bool;
-
-    /**
-     * Generate a new auth code for the user.
-     */
-    public function generateAuthCode(): AuthCode;
 }
