ac0d58539cf297206f655968202dcd8bd7dabdb3
D. Nagy Gergő <hello@iamgergo.com>
2025-12-30 23:04:51 +0100

    wip

--- resources/views/sitemap.blade.php
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-  <url>
-    <loc>https://www.example.com/product?id=123</loc>
-    <xhtml:link rel="alternate" hreflang="en" href="https://www.example.com/product?id=123&amp;lang=en"/>
-    <xhtml:link rel="alternate" hreflang="fr" href="https://www.example.com/product?id=123&amp;lang=fr"/>
-  </url>
-</urlset>
